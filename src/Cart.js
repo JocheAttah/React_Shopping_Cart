60a37fb0fed08912a9916dfd02cc12829c7bd0bb
Attah David 'Joche <39128641+JocheAttah@users.noreply.github.com>
2020-05-14 02:21:20 +0100

    implemented Grand Total

--- src/Cart.js
+++ src/Cart.js
@@ -2,16 +2,24 @@ import React from 'react';
 import CartItem from './CartItem';
 
 function Cart({initialItems}){
+  
+  const grandTotal = initialItems.reduce((total, item) => (
+    
+    total + (item.qty * item.price))
+    ,0).toFixed(2)
+
+
   return(
     <div className='Cart'>
       <h1> Shopping Cart</h1>
       <hr/>
       <div>
         {initialItems.map(item => (
-          <CartItem {...item}
-          /> 
+          <CartItem key= {item.iid} {...item} /> 
         ))}
       </div>
+
+      <h3>Grand Total: ${grandTotal} </h3>
 
      
     </div>
